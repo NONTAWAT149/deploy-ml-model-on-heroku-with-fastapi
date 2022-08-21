cf658ef3b2ea356ca27cc5e6dbbf419336cb1756
NONTAWAT149 <nontawat149@gmail.com>
2022-08-21 22:00:34 +0100

    update api and api test

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,32 @@
 # Put the code for your API here.
+from fastapi import FastAPI
+from schema import ModelInput
+from ml.model import inference
+from ml.data import clean_data
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "ML model is ready to use"}
+
+# This allows sending of data (our TaggedItem) via POST to the API.
+@app.post("/prediction/{data}")
+async def model_inference(data: ModelInput):
+    data = clean_data(data)
+    data = data[['age',
+                'workclass',
+                'fnlgt',
+                'education',
+                'education-num',
+                'marital-status',
+                'occupation',
+                'relationship',
+                'race',
+                'sex',
+                'capital-gain',
+                'capital-loss',
+                'hours-per-week',
+                'native-country']]
+    return {"prediction": inference(data)}
---
+++ starter/test_api.py
@@ -0,0 +1,52 @@
+from main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_get_root():
+    response = client.get("/")
+    assert response.status_code == 200, "response not successful"
+    assert response.json() == {"message": "ML model is ready to use"}, "wrong message gotten"
+
+def test_post_inference_one():
+    input_data = {'age': 76,
+                 'workclass': 'Private',
+                 'fnlgt': 124191,
+                 'education': 'Masters',
+                 'education-num': 14,
+                 'marital-status': 'Married-civ-spouse',
+                 'occupation': 'Exec-managerial',
+                 'relationship': 'Husband',
+                 'race': 'White',
+                 'sex': 'Male',
+                 'capital-gain': 0,
+                 'capital-loss': 0,
+                 'hours-per-week': 40,
+                 'native-country': 'United-States'}
+
+    response = client.post("/prediction/", json=input_data)
+    assert response.status_code == 200, "response not successful"
+    assert response.json() == {"prediction":'>50K'}, "got wrong prediction, expect '>50K'"
+
+
+def test_post_inference_two():
+    input_data = {'age': 22,
+                 'workclass': 'Private',
+                 'fnlgt': 201490,
+                 'education': 'HS-grad',
+                 'education-num': 9,
+                 'marital-status': 'Never-married',
+                 'occupation': 'Adm-clerical',
+                 'relationship': 'Own-child',
+                 'race': 'White',
+                 'sex': 'Male',
+                 'capital-gain': 0,
+                 'capital-loss': 0,
+                 'hours-per-week': 20,
+                 'native-country': 'United-States'}
+
+    response = client.post("/prediction/", json=input_data)
+    assert response.status_code == 200, "response not successful"
+    assert response.json() == {"prediction":'<=50K'}, "got wrong prediction, expect '<=50K'"
+
+
