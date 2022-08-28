014728609aec188a1263eeaa3bff515ec71b9044
NONTAWAT149 <nontawat149@gmail.com>
2022-08-29 00:11:27 +0100

    correct message

--- starter/main.py
+++ starter/main.py
@@ -16,7 +16,7 @@ class dataInput(BaseModel):
     race: str
     sex: str
     capital_gain: int = Field(alias='capital-gain')
-    capital_loss: int = Field(alias='capital-loss)
+    capital_loss: int = Field(alias='capital-loss')
     hours_per_week: int = Field(alias='hours-per-week')
     native_country: str = Field(alias='native-country')
 
