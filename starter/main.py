92cb744c6bdc833e4b72b0d6280005cf7c1a40f5
NONTAWAT149 <nontawat149@gmail.com>
2022-08-29 00:09:23 +0100

    define the right column name

--- starter/main.py
+++ starter/main.py
@@ -9,16 +9,16 @@ class dataInput(BaseModel):
     workclass: str
     fnlgt: int
     education: str
-    education_num: int
-    marital_status: str
+    education_num: int = Field(alias='education-num')
+    marital_status: str = Field(alias='marital-status')
     occupation: str
     relationship: str
     race: str
     sex: str
-    capital_gain: int
-    capital_loss: int
-    hours_per_week: int
-    native_country: str
+    capital_gain: int = Field(alias='capital-gain')
+    capital_loss: int = Field(alias='capital-loss)
+    hours_per_week: int = Field(alias='hours-per-week')
+    native_country: str = Field(alias='native-country')
 
 
 app = FastAPI()
